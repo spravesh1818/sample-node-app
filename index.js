78a0be29b6201a7383c424076067923401065154
Pravesh Chapagain <p.chapagain@krispcall.com>
2022-01-12 09:19:05 +0545

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express=require('express');
+
+const app=express();
+const port=3000;
+
+app.get('/',(req,res)=>{res.send("Hello World");})
+
+var server=app.listen(port,()=>{
+    console.log("App listening at port %s",port)
+});
